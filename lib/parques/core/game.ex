f3cbf8277dfd820d7d0d8d04fe2e1afad7fff750
Jhon Pedroza <jhon@pedroza.me>
2022-03-27 01:44:26 -0500

    Add name to game and set_name/2

--- lib/parques/core/game.ex
+++ lib/parques/core/game.ex
@@ -7,11 +7,17 @@ defmodule Parques.Core.Game do
   alias Parques.Core.Player
 
   typedstruct enforce: true do
+    field :name, String.t(), default: "New game"
     field :players, [Player.t()], default: []
   end
 
   @spec new(Enum.t()) :: t()
   def new(fields \\ []) do
     struct!(__MODULE__, fields)
+  end
+
+  @spec set_name(t(), String.t()) :: t()
+  def set_name(game, new_name) do
+    %{game | name: new_name}
   end
 end
--- test/parques/core/game_test.exs
+++ test/parques/core/game_test.exs
@@ -8,7 +8,23 @@ defmodule Parques.Core.GameTest do
     test "creates a game" do
       %Game{} = game = Game.new()
 
+      assert is_binary(game.name)
       assert game.players == []
     end
+  end
+
+  describe "set_name/2" do
+    setup [:game]
+
+    test "changes the name of the game", %{game: game} do
+      new_name = "A game with friends"
+      game = Game.set_name(game, new_name)
+
+      assert game.name == new_name
+    end
+  end
+
+  defp game(_context) do
+    {:ok, game: build(:game)}
   end
 end
