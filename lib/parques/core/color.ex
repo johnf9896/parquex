d6ca2bb76607d991459ac546449deee508d7776b
Jhon Pedroza <jhon@pedroza.me>
2022-03-27 02:34:36 -0500

    Add some Color functions with tests

--- lib/parques/core/color.ex
+++ lib/parques/core/color.ex
@@ -3,5 +3,24 @@ defmodule Parques.Core.Color do
   Defines the possible colors for players
   """
 
+  @colors [:red, :green, :blue, :yellow]
+  @count length(@colors)
+
   @type t :: :red | :green | :blue | :yellow
+
+  @spec count :: pos_integer()
+  def count, do: @count
+
+  @spec list :: [t()]
+  def list, do: @colors
+
+  @spec first :: t()
+  def first, do: hd(@colors)
+
+  @spec available([t()]) :: t() | nil
+  def available(taken) when length(taken) <= @count do
+    List.first(@colors -- taken)
+  end
+
+  defguard is_color(color) when color in @colors
 end
---
+++ test/parques/core/color_test.exs
@@ -0,0 +1,72 @@
+defmodule Parques.Core.ColorTest do
+  @moduledoc false
+  use Parques.Case, async: true
+
+  require Parques.Core.Color
+
+  alias Parques.Core.Color
+
+  describe "count/0" do
+    test "returns the number of colors" do
+      assert Color.count() == 4
+    end
+  end
+
+  describe "list/0" do
+    test "returns list of colors of known size" do
+      colors = Color.list()
+
+      assert length(colors) == Color.count()
+
+      for color <- colors do
+        assert is_atom(color)
+      end
+    end
+  end
+
+  describe "first/1" do
+    test "returns the first color" do
+      assert Color.first() == hd(Color.list())
+      assert Color.first() == hd(Color.list())
+    end
+  end
+
+  describe "available/1" do
+    test "returns the first color when none is taken" do
+      assert Color.available([]) == Color.first()
+    end
+
+    test "returns nil when all are taken" do
+      assert Color.available(Color.list()) == nil
+    end
+
+    test "returns a color until none is available" do
+      taken_color = some_color()
+
+      returned_colors =
+        for _i <- 1..(Color.count() - 1), reduce: [taken_color] do
+          taken_colors ->
+            another_color = Color.available(taken_colors)
+            assert another_color not in taken_colors
+
+            [another_color | taken_colors]
+        end
+
+      assert MapSet.new(returned_colors) == MapSet.new(Color.list())
+    end
+  end
+
+  describe "is_color/1" do
+    test "returns true for a color" do
+      color = some_color()
+
+      assert Color.is_color(color) == true
+    end
+
+    test "return false for something else" do
+      assert Color.is_color(nil) == false
+    end
+  end
+
+  defp some_color, do: Enum.random(Color.list())
+end
